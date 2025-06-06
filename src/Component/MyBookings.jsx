6f0343314aace8b53c66a2c9b1828b7155f16d86
Tawhid <tawhidh.b10@gmail.com>
2025-06-06 23:31:26 +0600

    add the my details

--- src/Component/MyBookings.jsx
+++ src/Component/MyBookings.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const MyBookings = () => {
     return (
+        <>
+        <Helmet>
+                <title>My-Booking</title>
+                <meta name="description" content="Helmet application" />
+            </Helmet>
         <div>
             <h1 className=' mt-25'>this is my bookings</h1>
         </div>
+        </>
     );
 };
 
