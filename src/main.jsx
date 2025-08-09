a5e602e1b6fafdd9af89b81c96c1e5faf6f143c2
Tawhid <tawhidh.b10@gmail.com>
2025-08-09 12:10:14 +0600

    polish details tost

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'react-toastify/dist/ReactToastify.css' // ✅ Toast CSS import
+import { ToastContainer } from 'react-toastify' // ✅ Toast component import
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import AuthProvider from './Provider/AuthProvider.jsx'
@@ -22,6 +24,7 @@ createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <RouterProvider router={router} />
+        <ToastContainer position="top-center" autoClose={3000} /> {/* ✅ Toast container */}
       </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
